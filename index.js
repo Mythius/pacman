2f344ab201879cf006599265a2a11df174c44115
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2021-01-10 13:13:02 -0700

    Add Icon

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const name = "pacman";
 
 function createWindow () {
   let win = new BrowserWindow({
-    width: 600,
-    height: 400,
+    width: 700,
+    height: 500,
     webPreferences: {
       nodeIntegration: true
     },
-    // icon: __dirname + '/favicon.ico'
+    icon: __dirname + '/favicon.ico'
   })
   if (process.argv.length > 1 && process.argv[1] != '.') {
       const shortcutsCreated = createDesktopShortcut({
           windows: { 
               filePath: `%appdata%\\..\\local\\${name}\\${name}.exe`,
-              // icon: __dirname + '\\favicon.ico'
+              icon: __dirname + '\\favicon.ico'
           }
       });
   }
@@ -24,12 +24,13 @@ function createWindow () {
 
 function createWindow () {
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 820,
+    height: 638,
     webPreferences: {
       nodeIntegration: true
-    }
-    // icon: __dirname + '/site/favicon.ico'
+    },
+    // frame: false
+    icon: __dirname + '/site/favicon.ico'
   });
 
   win.loadFile('site/index.html');
